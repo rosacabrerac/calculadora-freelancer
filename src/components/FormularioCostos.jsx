50e317a4b85d65dc191c777bea3cca365ca120ab
Rosa Cabrera <mrcabreracorrea@gmail.com>
2026-06-22 18:22:19 -0300

    feat: connect FormularioCostos inputs to props

--- src/components/FormularioCostos.jsx
+++ src/components/FormularioCostos.jsx
@@ -1,23 +1,51 @@
-function FormularioCostos() {
+function FormularioCostos({ costos, handleChange }) {
   return (
     <form>
       <div>
-        <label htmlFor="salario-deseado">Salario Deseado:</label>
-        <input type="number" id="salario-deseado" min="0" />
+        <label htmlFor="salarioDeseado">Salario Deseado:</label>
+        <input
+          type="number"
+          id="salarioDeseado"
+          min="0"
+          name="salarioDeseado"
+          value={costos.salarioDeseado}
+          onChange={handleChange}
+        />
       </div>
       <div>
-        <label htmlFor="gastos-fijos">Gastos Fijos Mensuales:</label>
-        <input type="number" id="gastos-fijos" min="0" />
+        <label htmlFor="gastosFijos">Gastos Fijos Mensuales:</label>
+        <input
+          type="number"
+          id="gastosFijos"
+          min="0"
+          name="gastosFijos"
+          value={costos.gastosFijos}
+          onChange={handleChange}
+        />
       </div>
       <div>
-        <label htmlFor="horas-trabajo">Horas de Trabajo:</label>
-        <input type="number" id="horas-trabajo" min="0" />
+        <label htmlFor="horasTrabajo">Horas de Trabajo:</label>
+        <input
+          type="number"
+          id="horasTrabajo"
+          min="0"
+          name="horasTrabajo"
+          value={costos.horasTrabajo}
+          onChange={handleChange}
+        />
       </div>
       <div>
-        <label htmlFor="porcentaje-imprevistos">
+        <label htmlFor="porcentajeImprevistos">
           Porcentaje de Imprevistos:
         </label>
-        <input type="number" id="porcentaje-imprevistos" min="0" />
+        <input
+          type="number"
+          id="porcentajeImprevistos"
+          min="0"
+          name="porcentajeImprevistos"
+          value={costos.porcentajeImprevistos}
+          onChange={handleChange}
+        />
       </div>
       <button type="submit">Enviar</button>
     </form>
