7da914689f12a08fe7bd5e43593d1268858f55c7
Rosa Cabrera <mrcabreracorrea@gmail.com>
2026-06-23 14:48:59 -0300

    feat(form): reorganize inputs into fieldsets and add new cost fields

--- src/components/FormularioCostos.jsx
+++ src/components/FormularioCostos.jsx
@@ -1,7 +1,41 @@
 function FormularioCostos({ costos, handleChange }) {
   return (
     <form>
-      <div>
+      <fieldset>
+        <legend>Costos Fijos</legend>
+        <label htmlFor="conectividadServicios">Conectividad y Servicios:</label>
+        <input
+          type="number"
+          id="conectividadServicios"
+          min="0"
+          name="conectividadServicios"
+          value={costos.conectividadServicios}
+          onChange={handleChange}
+        />
+
+        <label htmlFor="suscripcionesSoftware">Suscripciones y Software:</label>
+        <input
+          type="number"
+          id="suscripcionesSoftware"
+          min="0"
+          name="suscripcionesSoftware"
+          value={costos.suscripcionesSoftware}
+          onChange={handleChange}
+        />
+
+        <label htmlFor="espacioAlquiler">Espacio de Trabajo / Alquiler:</label>
+        <input
+          type="number"
+          id="espacioAlquiler"
+          min="0"
+          name="espacioAlquiler"
+          value={costos.espacioAlquiler}
+          onChange={handleChange}
+        />
+      </fieldset>
+
+      <fieldset>
+        <legend>Metas Financieras</legend>
         <label htmlFor="salarioDeseado">Salario Deseado:</label>
         <input
           type="number"
@@ -11,30 +45,17 @@ function FormularioCostos({ costos, handleChange }) {
           value={costos.salarioDeseado}
           onChange={handleChange}
         />
-      </div>
-      <div>
-        <label htmlFor="gastosFijos">Gastos Fijos Mensuales:</label>
+
+        <label htmlFor="ahorroMensual">Ahorro Mensual:</label>
         <input
           type="number"
-          id="gastosFijos"
+          id="ahorroMensual"
           min="0"
-          name="gastosFijos"
-          value={costos.gastosFijos}
+          name="ahorroMensual"
+          value={costos.ahorroMensual}
           onChange={handleChange}
         />
-      </div>
-      <div>
-        <label htmlFor="horasTrabajo">Horas de Trabajo:</label>
-        <input
-          type="number"
-          id="horasTrabajo"
-          min="0"
-          name="horasTrabajo"
-          value={costos.horasTrabajo}
-          onChange={handleChange}
-        />
-      </div>
-      <div>
+
         <label htmlFor="porcentajeImprevistos">
           Porcentaje de Imprevistos:
         </label>
@@ -46,8 +67,30 @@ function FormularioCostos({ costos, handleChange }) {
           value={costos.porcentajeImprevistos}
           onChange={handleChange}
         />
-      </div>
-      <button type="submit">Enviar</button>
+      </fieldset>
+
+      <fieldset>
+        <legend>Tiempo Disponible</legend>
+        <label htmlFor="horasSemanales">Horas de Trabajo Semanales:</label>
+        <input
+          type="number"
+          id="horasSemanales"
+          min="0"
+          name="horasSemanales"
+          value={costos.horasSemanales}
+          onChange={handleChange}
+        />
+
+        <label htmlFor="vacacionesSemanas">Semanas de Vacaciones al Año:</label>
+        <input
+          type="number"
+          id="vacacionesSemanas"
+          min="0"
+          name="vacacionesSemanas"
+          value={costos.vacacionesSemanas}
+          onChange={handleChange}
+        />
+      </fieldset>
     </form>
   );
 }
